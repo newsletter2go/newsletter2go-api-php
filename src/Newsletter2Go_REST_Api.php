e69d74aa0ad9c1bbce1637353c8c8d453f4b5e27
Bastian Konetzny <bkonetzny@users.noreply.github.com>
2017-12-08 07:57:59 +0100

    Added retry logic to call if token gets invalid

--- src/Newsletter2Go_REST_Api.php
+++ src/Newsletter2Go_REST_Api.php
@@ -278,7 +278,6 @@ class Newsletter2Go_REST_Api
     	
     }
 
-
     /**
      * @param $endpoint string the endpoint to call (see docs.newsletter2go.com)
      * @param $data array tha data to submit. In case of POST and PATCH its submitted as the body of the request. In case of GET and PATCH it is used as GET-Params. See docs.newsletter2go.com for supported parameters.
@@ -294,7 +293,16 @@ class Newsletter2Go_REST_Api
         if (!isset($this->access_token) || strlen($this->access_token) == 0) {
             throw new \Exception("Authentication failed");
         }
-        return $this->_curl('Bearer ' . $this->access_token, $endpoint, $data, $type);
+
+        $apiReponse = $this->_curl('Bearer ' . $this->access_token, $endpoint, $data, $type);
+
+        // check if token is expired
+        if (isset($apiReponse->error) && $apiReponse->error == "invalid_grant") {
+            $this->getToken();
+            $apiReponse = $this->_curl('Bearer ' . $this->access_token, $endpoint, $data, $type);
+        }
+
+        return $apiReponse;
     }
 
     private function _curl($authorization, $endpoint, $data, $type = "GET")
